8889f25ab14a87ea4a241831ef7b37e3a0f7d410
adelonmedeiros <adelon.medeiros@gmail.com>
2021-04-26 23:07:55 -0300

    app finalized!

--- App.tsx
+++ App.tsx
@@ -1,6 +1,21 @@
 import React from "react";
-import { Welcome } from "./src/pages/welcome";
+import AppLoading from "expo-app-loading";
+
+import Routes from "./src/routes/index ";
+
+import {
+  useFonts,
+  Jost_500Medium,
+  Jost_700Bold,
+} from "@expo-google-fonts/jost";
 
 export default function App() {
-  return <Welcome />;
+  const [fontsLoaded] = useFonts({
+    Jost_500Medium,
+    Jost_700Bold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
+  return <Routes />;
 }
---
+++ src/styles/fonts.ts
@@ -0,0 +1,5 @@
+export default {
+    heading: 'Jost_700Bold',
+    text: 'Jost_500Medium',
+    complement: 'Jost_500Medium'
+};
